f5f7800bb22bfed714df78d1a5f54e39cb0519c8
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-20 15:32:24 -0300

    1/2 da classe Order

--- src/model/entities/Order.java
+++ src/model/entities/Order.java
@@ -1,5 +1,74 @@
 package model.entities;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import model.enums.OrderStatus;
+
+public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> orderItem = new ArrayList<>();
+	
+	public Order() {
+		
+	}
+	
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		this.orderItem.add(orderItem);
+	}
+	
+	public void removeOrderItem(OrderItem orderItem) {
+		this.orderItem.remove(orderItem);
+	}
+	
+	public Double total() {
+		double sum = 0.0;
+		for (OrderItem item : orderItem) {
+			sum += item.getPrice();
+		}
+		return sum;
+	}
+	
+	
+	
+	
 }
