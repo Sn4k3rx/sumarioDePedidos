b1a6d29c46825074dc8be4f9fb2d733c268bdab2
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-20 14:38:57 -0300

    Realização e conclusão da classe Client

--- src/model/entities/Client.java
+++ src/model/entities/Client.java
@@ -1,5 +1,44 @@
 package model.entities;
 
-public class Client {
+import java.util.Date;
 
+public class Client {
+	
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	public Client() {
+		
+	}
+	
+	public Client(String name, String email, String bithDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 }
